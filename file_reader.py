8203ab22d5f8cbdc19be94cbf4d3347db08633ef
Avijit Saha <avijit.saha@watercoporation.com>
2021-05-21 18:48:38 +0530

    Adding File Reader Code

---
+++ file_reader.py
@@ -0,0 +1,37 @@
+"""
+
+This python module will read all '.txt' files in a directory.
+
+"""
+# Import Module
+import os
+
+# Folder Path "Enter Folder Path"
+path = "C:/pytest"
+
+# Change the directory
+os.chdir(path)
+
+
+# Read text File
+def read_text_file(file_path):
+	with open(file_path, 'r') as f:
+		print(f.read())
+
+
+# main
+def main():
+
+	# iterate through all file
+	for file in os.listdir():
+		# Check whether file is in text format or not
+		if file.endswith(".txt"):
+			file_path = f"{path}\{file}"
+
+			# call read text file function
+			read_text_file(file_path)
+
+
+if __name__ == "__main__":
+	main()
+
